1d1e3e72e35807d89d947a0cac97c7228d2223e0
Av981 <86481928+Av981@users.noreply.github.com>
2022-02-28 17:34:10 +0530

    set the width

--- src/components/Roical.js
+++ src/components/Roical.js
@@ -3,7 +3,7 @@ import { IoIosClose } from "react-icons/io";
 import { BiPencil } from "react-icons/bi";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import { ToastContainer, toast } from "react-toastify";
-import  {BsCheck2}  from "react-icons/bs";
+import { BsCheck2 } from "react-icons/bs";
 import "react-toastify/dist/ReactToastify.css";
 import "./roical.css";
 
@@ -16,13 +16,13 @@ function Roical(props) {
   const [usd, setUsd] = useState(true);
   const [time, setTime] = useState(0.0);
   const [apy, setApy] = useState(true);
-  const [show,setShow]=useState(true);
-  const[pencil,setPencil]=useState(true);
+  const [show, setShow] = useState(true);
+  const [pencil, setPencil] = useState(true);
   const setApyBtn = () => {
     if (apy) {
       toast("your are entering accelerated APY mode...", {
         position: "top-center",
-      }); 
+      });
     } else {
       toast("you are existing from acceletated APY mode...", {
         position: "top-center",
@@ -52,8 +52,7 @@ function Roical(props) {
     const name = e.target.name;
     const value = e.target.value;
     console.log(name, value);
-      setVal({ ...val, [name]: value });
-  
+    setVal({ ...val, [name]: value });
   };
   let x;
   const setDay = (e) => {
@@ -181,97 +180,126 @@ function Roical(props) {
         </div>
 
         <div className="cake curr-rate"> ROI at current rate</div>
-        {
-          pencil ?
+        {pencil ? (
           <div className="roi-output-forpen" title="You Will Get">
-         { 
-          pencil ?
-          <div className="roi-output-pencil">
-          <div className="pencil" title="pencil">
-             <button onClick={()=>{ setPencil(false);
-            toast("you are toogle to check...", {
-              position: "top-center",
-            });
-          }}> 
-             <BiPencil size={20}
-             /> 
-              </button> 
+            {pencil ? (
+              <div className="roi-output-pencil">
+                <div className="pencil" title="pencil">
+                  <button
+                    onClick={() => {
+                      setPencil(false);
+                      toast("you are toogle to check...", {
+                        position: "top-center",
+                      });
+                    }}
+                  >
+                    <BiPencil size={20} />
+                  </button>
+                </div>
+                <div> {time} USD </div>
               </div>
-              <div> {time} USD </div>
-           </div>
-        :
-        <div className="roi-output-check">
-          <div className="pencil-check" title="check"> <button onClick={()=>{setPencil(true);
-          toast("you are toggle to pencil ...", {
-            position: "top-center",
-          });
-          }}> < BsCheck2 size={20}/></button> </div>
-         <div> {time} USD </div>
-        </div>
-         }
-         </div>
-
-        
-
-        :
-        <div className="roi-output" title="You Will Get">
-         { 
-          pencil ?
-          <div className="roi-output-pencil">
-          <div className="pencil" title="pencil">
-             <button onClick={()=>{ setPencil(false);
-            toast("you are toogle to check...", {
-              position: "top-center",
-            });
-          }}> 
-             <BiPencil size={20}
-             /> 
-              </button> 
+            ) : (
+              <div className="roi-output-check">
+                <div className="pencil-check" title="check">
+                  {" "}
+                  <button
+                    onClick={() => {
+                      setPencil(true);
+                      toast("you are toggle to pencil ...", {
+                        position: "top-center",
+                      });
+                    }}
+                  >
+                    {" "}
+                    <BsCheck2 size={20} />
+                  </button>{" "}
+                </div>
+                <div> {time} USD </div>
               </div>
-              <div> {time} USD </div>
-           </div>
-        :
-        <div className="roi-output-check">
-          <div className="pencil-check" title="check"> <button onClick={()=>{setPencil(true);
-          toast("you are toggle to pencil ...", {
-            position: "top-center",
-          });
-          }}> < BsCheck2 size={20}/></button> </div>
-         <div> {time} USD </div>
-        </div>
-         }
-         </div>
-         }
-        <small className="cake curr-rate">~0.000 CAKE + 0.000000 DON </small>
+            )}
+          </div>
+        ) : (
+          <div className="roi-output" title="You Will Get">
+            {pencil ? (
+              <div className="roi-output-pencil">
+                <div className="pencil" title="pencil">
+                  <button
+                    onClick={() => {
+                      setPencil(false);
+                      toast("you are toogle to check...", {
+                        position: "top-center",
+                      });
+                    }}
+                  >
+                    <BiPencil size={20} />
+                  </button>
+                </div>
+                <div> {time} USD </div>
+              </div>
+            ) : (
+              <div className="roi-output-check">
+                <div className="pencil-check" title="check">
+                  {" "}
+                  <button
+                    onClick={() => {
+                      setPencil(true);
+                      toast("you are toggle to pencil ...", {
+                        position: "top-center",
+                      });
+                    }}
+                  >
+                    {" "}
+                    <BsCheck2 size={20} />
+                  </button>{" "}
+                </div>
+                <div> {time} USD </div>
+              </div>
+            )}
+          </div>
+        )}
+        <small 
+        className="cake curr-rate"
+        >
+          ~0.000 CAKE + 0.000000 DON{" "}
+        </small>
         <div className="apply-cancel-btn">
           <button className="apply-btn"> Apply</button>
           <button className="cancel-btn">Cancel</button>
         </div>
-        {
-          show?<div>
-           <div className=" hide-details">
-           <button onClick={()=>setShow(false)} ><h3> Hide Details <FiChevronDown /> </h3></button> 
-         </div>
-          <div className="apy">
-          <div> APY</div>
-          <div id="colour">68.0%</div>
-        </div>
-        <div className="hide-content">
-          <ul>
-            <li> Calculated based on current rate</li>
-            <li>
-              All figures ase estimates provided for your convenience only,and
-              by no means represent guaranteed returns.
-            </li>
-          </ul>
-        </div>
-        </div>
-         :        
-          <div className="show-details"> 
-             <button onClick={()=>setShow(true)}> <h3>Show Details <FiChevronUp/></h3> </button>
-           
-         </div> 
-        }      
+        {show ? (
+          <div>
+            <div className=" hide-details">
+              <button onClick={() => setShow(false)}>
+                <h3>
+                  {" "}
+                  Hide Details <FiChevronDown />{" "}
+                </h3>
+              </button>
+            </div>
+            <div className="apy">
+              <div> APY</div>
+              <div id="colour">68.0%</div>
+            </div>
+            <div className="hide-content">
+              <ul>
+                <li> Calculated based on current rate</li>
+                <li>
+                  All figures ase estimates provided for your convenience
+                  only,and by no means represent guaranteed returns.
+                </li>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <div className="show-details">
+            <button onClick={() => setShow(true)}>
+              {" "}
+              <h3>
+                Show Details <FiChevronUp />
+              </h3>{" "}
+            </button>
+          </div>
+        )}
       </div>
       <ToastContainer />
     </>
